0717f68015893704b67e728dcaabaf0ef41db60a
Aswin K R <116720043+AlphaSpider@users.noreply.github.com>
2025-11-05 02:18:13 +0530

    Displayed Bloglist
    Displayed Blog-list as a separate component with props

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,17 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-    // let name='mario';
-    const [name, setName] = useState('mario');
-    const [age, setAge] = useState(23);
-
-    const handleClick = ()=> {
-        setName('Laila')
-        setAge(24)
-
-    }
-
-
-
+    const[blogs, setBlog]=useState([
+        {title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1},
+        {title: 'Welcome party', body: 'lorem ipsum...', author: 'alice', id: 2},
+        {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'bob', id: 3},
+    ]); 
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{name} is {age} years old.</p>
-            <button onClick={handleClick}>Click me</button>
+            <BlogList blogs={blogs}/>
         </div>
      );
-}
- 
+} 
+
 export default Home;
