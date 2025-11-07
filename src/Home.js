4a1c4c50d3b6287a78db7aa671001c9b5bcf2fd0
Aswin K R <116720043+AlphaSpider@users.noreply.github.com>
2025-11-07 23:41:24 +0530

    Setup delete button and function

--- src/Home.js
+++ src/Home.js
@@ -2,14 +2,20 @@ import { useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const[blogs, setBlog]=useState([
+    const[blogs, setBlogs]=useState([
         {title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1},
         {title: 'Welcome party', body: 'lorem ipsum...', author: 'alice', id: 2},
         {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'bob', id: 3},
     ]); 
+
+    const handleDelete=(id)=>{
+        const newBlogs=blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
     return ( 
         <div className="home">
-            <BlogList blogs={blogs}/>
+            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
         </div>
      );
 } 
